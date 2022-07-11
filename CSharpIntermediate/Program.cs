9725b7898ba9851baeaa4161a3206accd97709dd
James Grieve <jgrieve@ualberta.ca>
2022-07-11 09:50:51 -0600

    Read-only / derived properties;

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,8 +2,10 @@
 
 
 Dog aDog = new Dog("Fido", "Golden Retriever", 3);
+Dog anotherDog = new Dog("Max", "Black Labrador", 4);
 
 Console.WriteLine(aDog.Name + " has been alive for " + aDog.HumanAge + " years, which roughly equates to the maturity of a " + aDog.DogAge + " year old human.");
+Console.WriteLine(anotherDog.Name + " has been alive for " + anotherDog.HumanAge + " years, which roughly equates to the maturity of a " + anotherDog.DogAge + " year old human.");
 
 
 public class Dog { 
@@ -19,6 +21,7 @@ public class Dog {
     public int HumanAge { get; set; }
    
     // Read-only (derived) properties are properties with no setter, they merely return a value derived from other values.
+    // Attempting to set a read-only property will result in an error.
     public int DogAge
     {
         get
