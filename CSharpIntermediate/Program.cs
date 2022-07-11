d144dbc9cff9534f96f0bb822e2e01b9639b7ac5
James Grieve <jgrieve@ualberta.ca>
2022-07-11 09:27:58 -0600

    Basic objects and properties;

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MyFirstClass myFirstObject = new MyFirstClass();
+
+myFirstObject.myCharProp = '!';
+
+public class MyFirstClass
+{ 
+    // Constructor (instantiates the class).
+    public MyFirstClass()
+    {
+
+    }
+    public int myIntField;
+
+    // Auto-implemented property (meaning there is no additional validation or formatting, or any other logic applied to the accessor/mutator).
+    // It also hides the backing field.
+
+    // Behind this is something like:
+    // private string myStringField;
+    public string myStringProp { get; set; }
+
+
+    // Fully-implemented property:
+    private char myCharField;
+    public char myCharProp
+    {
+        get
+        {
+            return myCharField;
+        }
+        set
+        {
+            myCharField = value;
+        }
+    }
+}
+
+
