f0ecc69d691cc9180692cc10f0215268c46280c3
James Grieve <jgrieve@ualberta.ca>
2022-07-11 09:30:12 -0600

    Out;

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,37 +1,3 @@
-﻿MyFirstClass myFirstObject = new MyFirstClass();
-
-myFirstObject.myCharProp = '!';
-
-public class MyFirstClass
-{ 
-    // Constructor (instantiates the class).
-    public MyFirstClass()
-    {
-
-    }
-    public int myIntField;
-
-    // Auto-implemented property (meaning there is no additional validation or formatting, or any other logic applied to the accessor/mutator).
-    // It also hides the backing field.
-
-    // Behind this is something like:
-    // private string myStringField;
-    public string myStringProp { get; set; }
-
-
-    // Fully-implemented property:
-    private char myCharField;
-    public char myCharProp
-    {
-        get
-        {
-            return myCharField;
-        }
-        set
-        {
-            myCharField = value;
-        }
-    }
-}
+﻿// In-class: Create a class for a dog. It should have a property for a name (string), breed (string) and age (int).
 
 
