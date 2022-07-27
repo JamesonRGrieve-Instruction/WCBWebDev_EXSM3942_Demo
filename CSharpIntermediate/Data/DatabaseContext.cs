e0a4816d16b1c0c0397b65e395ac4ea2127fff15
James Grieve <jgrieve@ualberta.ca>
2022-07-27 09:46:47 -0600

    Initial model setup; Skeleton database context;

---
+++ CSharpIntermediate/Data/DatabaseContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 3. Apply the appropriate using statements:
+using Microsoft.EntityFrameworkCore;
+
+// 1. Change the namespace to the same as our models.
+namespace CSharpIntermediate.Models
+{
+    // 2. Change the default "internal" to "public", and apply the partial modifier.
+    // 4. Inherit from DbContext.
+    public partial class DatabaseContext : DbContext
+    {
+        // 5. Create constructors for both options and no options.
+        // This allows us to create a context either in the default state, or a customized one.
+        public DatabaseContext()
+        { 
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        // 6. Create database sets for each table we plan to include.
+        public virtual DbSet<Product> Products { get; set; }
+
+        // 7. Generate a OnConfiguring method:
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+        }
+
+        // 8. Generate a OnModelCreating method:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CSharpIntermediate/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 2. Add the using statements for EF:
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CSharpIntermediate.Models
+{
+    // 3. Name our table using an annotation (square brackets).
+    [Table("product")]
+    // 1. Change the default "internal" class to a "public" class.
+    public class Product
+    {
+        // 5. Apply annotations for the primary key:
+        [Key] // PRIMARY KEY
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Identity is Microsoft's version of AUTO_INCREMENT, EF translates this during migration.
+        [Column(TypeName = "int(10)")] // Data type for the column.
+        // 4. Create a property for the primary key.
+        public int ID { get; set; }
+
+        // 6. Create the rest of your columns (don't forget data types).
+        [Column(TypeName = "varchar(30)")]
+        public string? Name { get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int QuantityOnHand{ get; set; }
+
+        [Column(TypeName = "int(10)")]
+        public int? ReorderTheshold { get; set; }
+
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal SalePrice { get; set; }
+    }
+}
