d3bf0b2da360e8c7f3581c2b251e36714c69e098
James Grieve <jgrieve@ualberta.ca>
2022-07-28 12:16:51 -0600

    Migration;

---
+++ CSharpIntermediate/Migrations/20220728181547_InitialMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSharpIntermediate.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "manufacturer",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_manufacturer", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "model",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    manufacturer_id = table.Column<int>(type: "int(10)", nullable: false),
+                    name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false, collation: "utf8mb4_general_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_model", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Model_Manufacturer",
+                        column: x => x.manufacturer_id,
+                        principalTable: "manufacturer",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "manufacturer",
+                columns: new[] { "id", "name" },
+                values: new object[] { 1, "Chevrolet" });
+
+            migrationBuilder.InsertData(
+                table: "model",
+                columns: new[] { "id", "manufacturer_id", "name" },
+                values: new object[] { 1, 1, "Aveo" });
+
+            migrationBuilder.InsertData(
+                table: "model",
+                columns: new[] { "id", "manufacturer_id", "name" },
+                values: new object[] { 2, 1, "Corvette" });
+
+            migrationBuilder.CreateIndex(
+                name: "FK_Model_Manufacturer",
+                table: "model",
+                column: "manufacturer_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "model");
+
+            migrationBuilder.DropTable(
+                name: "manufacturer");
+        }
+    }
+}
