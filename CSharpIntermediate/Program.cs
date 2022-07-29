e4f8ddea6716b849427e0c52770724a3ee0d2021
James Grieve <jgrieve@ualberta.ca>
2022-07-29 11:45:24 -0600

    Demo;

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Object Instantiation
+Person johnPerson = new Person()
+{
+    FirstName = "John",
+    LastName = "Doe",
+    Age = 30,
+    BirthDate = new DateOnly(1992, 1, 1)
+};
+
+Person janePerson = new Person()
+{
+    FirstName = "Jane",
+    LastName = "Doe",
+    Age = 29,
+    BirthDate = new DateOnly(1993, 2, 2)
+};
+
+// Object Variable Assignment (Reference)
+Person a = janePerson;
+Person b = janePerson;
+
+// Object Property Mutation
+a.FirstName = "Joan";
+// Object Property Access
+Console.WriteLine(b.FirstName);
+
+// Class Declaration
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+
+    public DateOnly BirthDate { get; set; }
+}
+
