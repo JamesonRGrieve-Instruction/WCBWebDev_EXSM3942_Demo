b597d2e09818b4da085ce424622f14b85c14a9c3
James Grieve <jgrieve@ualberta.ca>
2022-08-05 10:44:56 -0600

    Additional comments;

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,6 +2,8 @@
 using CSharpIntermediate.Models;
 using Microsoft.EntityFrameworkCore;
 
+
+// SELECT Example
 // Any time we want to access the database, if we wrap it in a using statement it ensures that when we are done, the connection isn't hanging open.
 using (DatabaseContext context = new DatabaseContext())
 {
@@ -12,8 +14,10 @@ using (DatabaseContext context = new DatabaseContext())
     {
         // EXPLICIT LOADING
         // Explicit loading only loads associated records for specific records you're looking at. 
+        // Explicit loading is sort of equivalent to a SELECT * and providing a specific primary key value (one query per record).
         // Now that we have our products stored in memory outside of the database, we can command ef to load the associated data for the product category.
         context.Entry(product).Reference(x => x.ProductCategory).Load();
+        // SELECT * FROM ProductCategory WHERE ID IN (SELECT Product.CategoryID FROM Product WHERE Name = 'Milk');
         // Once we have the assocaited data, we can output the category info with the product in a WriteLine.
         Console.WriteLine(product.ProductCategory.Name + ": " + product.Name + " costs " + product.SalePrice);
     }
@@ -21,11 +25,16 @@ using (DatabaseContext context = new DatabaseContext())
 
     // EAGER LOADING
     // Eager loading includes ALL associated records for the entire data set. This can be a memory hog depending on your implementation.
+    // Eager loading is sort of equivalent to a SELECT * with a join to the target table (one query for everything).
     foreach (Product product in context.Products.Include(product => product.ProductCategory))
+    // SELECT * FROM Product INNER JOIN ProductCategory ON ProductCategory.ID = Product.CategoryID;
     {
         // Once we have the assocaited data, we can output the category info with the product in a WriteLine.
         Console.WriteLine(product.ProductCategory.Name + ": " + product.Name + " costs " + product.SalePrice);
     }
-
-
 }
+
+// INSERT Example
+
+
+// UPDATE Example
